a4f40e9cc8b74d692bd186333a35c52250d78b5c
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2020-10-16 12:06:26 +0300

    Get latest block info

--- backend-javascript/backend.js
+++ backend-javascript/backend.js
@@ -2,15 +2,43 @@ async function backend() {
   // Import
   const { ApiPromise, WsProvider } = require('@polkadot/api');
 
-  // Construct
-  const wsProvider = new WsProvider('ws://127.0.0.1:9944');
-  const api = new ApiPromise({ provider: wsProvider });
+  const client = async (provider) => {
+    // Setting up default provider if none provided
+    provider = provider === undefined ? 'wss://rpc.polkadot.io' : provider
 
-  // Wait until we are ready and connected
-  await api.isReady;
+    const wsProvider = new WsProvider(provider);
+    const client = new ApiPromise({ provider: wsProvider });
 
-  // Do something
-  console.log(api.genesisHash.toHex());
+    await client.isReady;
+
+    return client;
+  };
+
+  // CLI
+  if (process.argv[2] == '--help') {
+    console.log('Available options are:\n' +
+      '--help: Shows you this menu\n' +
+      '--latest-block [chain]: Returns latest block on `chain`. `chain` defaults to `kusama`\n' +
+      '--search-block-by-number [chain] [number]: Searches for block `number` on `chain`. Defaults to latest block on kusama\n' +
+      '--search-block-by-height [chain] [height]: Searches for block `height` on `chain`. Defaults to latest block on kusama\n'
+    );
+
+    process.exit(1);
+  } else if (process.argv[2] == '--latest-block') {
+    const chain = process.argv[3];
+
+    client(chain)
+      .then((api) => api.rpc.chain.getBlock())
+      .then((block) => {
+        console.log('Latest block:', block.toHuman())
+        process.exit(0);
+      });
+  }
+  else {
+    console.log('Unknown command')
+
+    process.exit(1);
+  }
 };
 
 backend();
